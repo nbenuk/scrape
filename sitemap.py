f20abd5876419a3552317a3ebdc15c0ddd5feb59
Ben N <ben@Bens-MacBook-Pro.local>
2022-05-02 15:06:14 +0100

    urls from robots.txt sitemap

---
+++ sitemap.py
@@ -0,0 +1,19 @@
+import csv
+from bs4 import BeautifulSoup
+import requests
+
+source = requests.get('http://example.python-scraping.com/sitemap.xml').text
+
+soup = BeautifulSoup(source, 'lxml')
+
+csv_file = open('urlset.csv', 'w')
+
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow(['url'])
+
+table = soup.find('urlset')
+for url in table.find_all('url'):
+    csv_writer.writerow([url.text])
+    print(url.text)
+    print()
+csv_file.close
